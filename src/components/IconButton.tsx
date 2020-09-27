116e5fc13775901ea4238d0599d0041a73e28be7
“koki0608” <kouki0608.job@gmail.com>
2020-09-27 21:18:22 +0900

    モーダル画面の実装

---
+++ src/components/IconButton.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { StyleSheet, TouchableOpacity, GestureResponderEvent } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+
+type Props = {
+    onPress: (event: GestureResponderEvent) => void
+    name: string
+    color: string
+}
+
+// return()の省略
+export const IconButton: React.FC<Props> = ({
+    onPress,
+    name,
+    color = '#000'
+}: Props) => {
+    return (
+        <TouchableOpacity onPress={onPress} style={styles.container}>
+            <Feather name={name} color={color} size={32} />
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 8,
+    }
+})
--- src/screens/ShopScreen.tsx
+++ src/screens/ShopScreen.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { StyleSheet, SafeAreaView } from 'react-native'
 /* components */
 import { ShopDetail } from '../components/ShopDetail'
+import { FloatingActionButton } from '../components/FloatingActionButton'
 /* types */
 import { RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
@@ -21,7 +22,11 @@ export const ShopScreen: React.FC<Props> = ({ navigation, route }) => {
 
     return (
         <SafeAreaView style={styles.container}>
-            <ShopDetail shop={shop}></ShopDetail>
+            <ShopDetail shop={shop} />
+            <FloatingActionButton
+                iconName='plus'
+                onPress={() => navigation.navigate('CreateReview', { shop })}
+            />
         </SafeAreaView>
     )
 }
